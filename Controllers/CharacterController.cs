62fe7168edd3538215f993b52317c06cf01d8d81
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-02-06 21:07:13 -0300

    method update

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,12 +10,24 @@ namespace Net.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        //get list of characters
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
 
-        [HttpGet(Name = "GetCharacter")]
-        public ActionResult<Character> Get()
+        };
+
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
         {
-            return Ok(knight);
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
